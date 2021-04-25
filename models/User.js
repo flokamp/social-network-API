bfa861d82089a6b6e48c64b698f2aa303b35be69
flokamp <74511633+flokamp@users.noreply.github.com>
2021-04-25 18:37:25 -0500

    added thought model and reaction schema

--- models/User.js
+++ models/User.js
@@ -1,33 +1,46 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const UserSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		match: [/.+@.+\..+/],
-	},
-	thoughts: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Thought",
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
 		},
-	],
-	friends: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "User",
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			match: [/.+@.+\..+/],
 		},
-	],
-});
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Thought",
+			},
+		],
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
 
 // get total count of user's friends
 UserSchema.virtual("friendCount").get(function () {
 	return this.friends.length;
 });
+
+const User = model("User", UserSchema);
+
+module.exports = User;
